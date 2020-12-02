d4683ee00773d62953c6e6df998b8e0e384114f0
ArtemKsenchuk <73600987+ArtemKsenchuk@users.noreply.github.com>
2020-12-02 19:44:49 +0200

    Add files via upload

---
+++ TestDirectory.bx/WatchingDir.php
@@ -0,0 +1,56 @@
+<?php
+error_reporting();
+//C:\Users\USER\server\OpenServer\domains\TestDirectory.bx
+//$currentDir = opendir(trim(fgets(STDIN)));
+
+function DirCheck($currentDir)
+{
+
+	$list = [
+		'dirs' => [],
+		'folder' => [],
+		'files' => []
+	];
+	while ($element = readdir($currentDir))
+	{
+		if (in_array($element, ['.', '..']))
+		{
+			continue;
+		}
+		if (is_dir($element))
+		{
+			$list['dirs'][] = $element;
+		}
+		if (is_file($element))
+		{
+			$list['files'][] = $element;
+			$list['files']["$element"]['filesize'] = filesize($element);
+
+		if (is_readable($element))
+		{
+			$list['files']["$element"]['is_readable'] = 'true';
+		}
+		if (is_writable($element))
+		{
+			$list['files']["$element"]['is_writable'] = 'true';
+		}
+
+		}
+		else
+		{
+		$list['folder'][] = $element;
+		if (is_readable($element))
+		{
+			$list['folder']["$element"]['is_readable'] = 'true';
+		}
+		if (is_writable($element))
+		{
+			$list['folder']["$element"]['is_writable'] = 'true';
+		}
+
+	}
+}
+closedir($currentDir);
+return ($list);
+}
+//var_dump(DirCheck($currentDir));
